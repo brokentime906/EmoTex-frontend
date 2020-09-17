e3f3b423adced4ca987d3ecb7c528d869b0828fd
brokentime906 <btime906@gmail.com>
2020-09-17 20:11:34 +0900

    0917

--- src/constant.js
+++ src/constant.js
@@ -1,2 +1,3 @@
-export const BASE_URL = "https://ced4129f2132.ngrok.io";
+// export const BASE_URL = "https://ced4129f2132.ngrok.io";
 // export const BASE_URL = "http://localhost:4000";
+export const BASE_URL = "https://emotex-backend.herokuapp.com";
